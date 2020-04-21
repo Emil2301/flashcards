dae5885cb38e33fd3302ec063f6d1ca96bece1bc
emil2301 <emil.kawalko@capgemini.com>
2020-04-21 16:08:24 +0200

    search add search service and fetch placeholder data

--- flashcards-frontend/src/app/search/search.component.ts
+++ flashcards-frontend/src/app/search/search.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SearchService } from '../search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
+  constructor(private searchService: SearchService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  sendInputValue(inputValue) {
+    this.searchService.searchFlashcard().subscribe(
+      (data) => {
+        console.log(data);
+      }
+    );
   }
-
 }
