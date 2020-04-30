8241cc44549a591629f3329e2c23ed7176455bd1
emil2301 <emil.kawalko@capgemini.com>
2020-04-30 15:35:40 +0200

    add-navigation - add working navigation

--- flashcards-frontend/src/app/app-routing.module.ts
+++ flashcards-frontend/src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { EditModeComponent } from './components/edit-mode/edit-mode.component';
+import { SearchComponent } from './components/search/search.component';
+import { SwitchLangComponent } from './components/switch-lang/switch-lang.component';
+import { ErrorComponent } from './components/error/error.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'switch-lang', component: SwitchLangComponent },
+	{ path: 'edit-mode', component: EditModeComponent },
+	{ path: '',   redirectTo: '/search', pathMatch: 'full' },
+	{ path: '**', component: ErrorComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- flashcards-frontend/src/app/app.module.ts
+++ flashcards-frontend/src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { SearchComponent } from './components/search/search.component';
+import { EditModeComponent } from './components/edit-mode/edit-mode.component';
+import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { SwitchLangComponent } from './components/switch-lang/switch-lang.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, SearchComponent],
+  declarations: [AppComponent, HomePageComponent, SearchComponent, EditModeComponent, ToolbarComponent, SwitchLangComponent, ErrorComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
